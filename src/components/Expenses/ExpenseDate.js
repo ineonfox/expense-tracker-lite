16206f42b64f3ab54875151eb8eecc0937ebc614
ineonfox <mykhailo.masliukh@gmail.com>
2023-03-04 16:46:30 +0100

    Project reorganized

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,6 +1,6 @@
 import './ExpenseDate.css';
 
-function ExpenseDate(props) {
+const ExpenseDate = props => {
   const day = props.date.toLocaleString("en-GB", { day: "2-digit" });
   const month = props.date.toLocaleString("en-GB", { month: "long" });
   const year = props.date.getFullYear();
